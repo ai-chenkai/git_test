008743cbcc3771cdb9f391c30d20b7a9f1aa69fd
chenkai <chenkai@.com>
2021-01-02 13:01:56 +0800

    分支上修改

--- git_test/src/main/java/com/chenkai/HelloGit.java
+++ git_test/src/main/java/com/chenkai/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("二次修改");
+        System.out.println("分支上创建分支");
     }
 }

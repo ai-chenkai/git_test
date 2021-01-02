e04004cc6acebda39d39da527581635c463f0823
chenkai <chenkai@.com>
2021-01-02 20:53:55 +0800

    string、integer、int之间的相互转化测试

--- git_test/src/main/java/com/chenkai/HelloGit.java
+++ git_test/src/main/java/com/chenkai/HelloGit.java
@@ -2,10 +2,25 @@ package com.chenkai;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("二次修改");
-        System.out.println("分支上创建分支");
-        System.out.println("分支上创建分支2");
+        //字符串转int
+        int a=Integer.parseInt("100");
+        System.out.println(a);
+        //int转字符串
+        int b=100;
+        String c=b+"";
+        System.out.println(c);
+        //int转Integer 装箱
+        Integer d=100;
+        System.out.println(d);
+        //Integer转int 拆箱
+        int e=d;
+        System.out.println(e);
+        //integer转字符串
+        String f=String.valueOf(d);
+        System.out.println(f);
+        //字符串转Integer
+        Integer g=Integer.valueOf("200");
+        System.out.println(g);
 
     }
 }

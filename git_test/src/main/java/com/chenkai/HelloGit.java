2c715c8dc9a676dc900e17611bac633760b7243e
chenkai <chenkai@.com>
2021-01-03 09:29:04 +0800

    添加日期时间格式的转化的练习代码

--- git_test/src/main/java/com/chenkai/HelloGit.java
+++ git_test/src/main/java/com/chenkai/HelloGit.java
@@ -1,7 +1,11 @@
 package com.chenkai;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HelloGit {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         //字符串转int
         int a=Integer.parseInt("100");
         System.out.println(a);
@@ -21,6 +25,17 @@ public class HelloGit {
         //字符串转Integer
         Integer g=Integer.valueOf("200");
         System.out.println(g);
+        //date类型转字符串类型时间格式
+        Date nowTime=new Date();
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+        System.out.println(sdf.format(nowTime));
+        //string类型转date类型
+        String passTime="2011-11-11 11:11:11";
+        SimpleDateFormat sdf2=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date time=sdf2.parse(passTime);
+        System.out.println(time);
+
+
 
     }
 }
